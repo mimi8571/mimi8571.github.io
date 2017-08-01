7623b4077acab620214b60d21f4537d3c1f3c52b
Megan Lau <moylau27@gmail.com>
2017-07-31 21:14:37 -0700

    add smooth scroll; fixed popup with modal

--- home/portfolio.js
+++ home/portfolio.js
@@ -1,5 +1,39 @@
+$('.smooth_scroll').on('click', function() {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+    
+    return false;
+});
+
+/*
 function popup_box() {
     var popup = document.getElementById("popup");
     popup.classList.toggle("visibility");
 }
+*/
+
+function modalClose() {
+    if (location.hash == '#modalBox') {
+        location.hash = '';
+    }
+}
+
+//Escape key to exit
+document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 27) {
+        modalClose();
+    }
+});
+
+//Does not close if click on modal container
+var modal = document.querySelector('#modalBox');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+
+modal.children[0].addEventListener('click', function(e) {
+    e.stopPropagation();
+}, false);
 
